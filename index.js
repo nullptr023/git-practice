c28f91ad9e2f460b20db009785ef76fcbeac216f
avillanueva <programmer9323@gmail.com>
2017-10-17 22:25:56 -0500

    add hide and unhide modal functionality

---
+++ index.js
@@ -0,0 +1,11 @@
+function showAuthForm() {
+	console.log('showAuthForm');
+	document.getElementById('login-form').style.display = 'none';
+	document.getElementById('two-auth-form').style.display = 'block';
+}
+
+function showLoginForm () {
+	console.log('showLoginForm');
+	document.getElementById('login-form').style.display = 'block';
+	document.getElementById('two-auth-form').style.display = 'none';
+}
